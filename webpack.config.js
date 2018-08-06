cafeeee286486889ec75c81f94c4049dc06a0518
Stanley Makazhe <devt3000@gmail.com>
2018-08-06 13:25:24 +0200

    Dev server set up in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -34,5 +34,10 @@ module.exports = {
             template: 'index.html'
         }),
         new CleanWebpackPlugin(['dist'])
-    ]
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        open: true,
+    }
 }
